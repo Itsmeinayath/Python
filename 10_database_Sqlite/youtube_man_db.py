2ee5fda3f0e45e041bcc83e6e2bc260e101e6c4e
Mohammed Inayath <mohammedinayath1258@gmail.com>
2024-11-20 12:17:28 +0530

    Refactor YouTube manager: replace old database implementation with a new SQLite version; enhance user interaction and feedback

--- 10_database_Sqlite/youtube_man_db.py
+++ 10_database_Sqlite/youtube_man_db.py
@@ -25,8 +25,6 @@ def update_video(video_id, new_name, new_time):
     cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?",(new_name,new_time,video_id))
     conn.commit()
 
-    
-
 def delete_video(video_id):
     cursor.execute("DELETE FROM videos WHERE id = ?",(video_id,))
     conn.commit()
@@ -41,32 +39,35 @@ def main():
        print("3. Update a youtube video details")
        print("4. delete a youtube video")
        print("5. Exit the application")
+       print("----------------------------")
        choice = input("Enter your choice: ")
-
-
        if choice == "1":
            list_all_videos()
+           print("----------------------------")
+        #    print(list_all_videos())
        elif choice == "2":
            name= input("Enter the title of the video: ")
            time = input("Enter video duration: ")
            add_video(name, time)
+           print("----------------------------")
+           print("Video added successfully")
        elif choice == "3":
            video_id = input("Enter the video number you want to update: ")
            name= input("Enter the title of the video: ")
            time = input("Enter video duration: ")
            update_video(video_id,name, time)
+           print("----------------------------")
+           print("Video updated successfully")
        elif choice == "4":
            video_id = input("Enter the video number you want to delete: ")
            delete_video(video_id)
+           print("----------------------------")
+           print("Video deleted successfully")
        elif choice == "5":
            break   
        else:
               print("Invalid choice. Please try again")
               continue 
-
-   conn.close() 
-            
-        
-
+   conn.close()            
 if __name__ == "__main__":
     main()
